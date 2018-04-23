4ea2568c9309eebd0c333719ad08ae0ebf689973
leny <leny@172.17.192.90>
2018-04-23 12:17:19 +0900

    Add_Faculty

---
+++ src/main/java/edu/handong/csee/java/example/inheritance/Faculty.java
@@ -0,0 +1,36 @@
+package edu.handong.csee.java.example.inheritance;
+
+public class Faculty extends Person{
+	private int facultyNumber;
+	   public Faculty() {
+		   super();
+		   facultyNumber = 0;
+	   }
+	   
+	   public Faculty(String initialName, int initialStudentNumber){
+			super(initialName);        
+			facultyNumber = initialStudentNumber;    
+		}
+	   
+	   public void reset(String newName, int newFacultyNumber) {
+			setName(newName);        
+			facultyNumber = newFacultyNumber;
+	   }
+	   
+	   public int getFacultyNumber() {
+		   return facultyNumber;
+	   }
+	   
+	   public void setFacultyNumber(int newFacultyNumber) {
+			facultyNumber = newFacultyNumber;
+		}
+	   
+	   public void writeOutput() {
+			System.out.println("Name: " + getName());
+			System.out.println("faculty Number: " + facultyNumber); 
+		}
+	   
+	   public boolean equals(Faculty otherFaculty) {
+			return this.hasSameName(otherFaculty) && (this.facultyNumber == otherFaculty.facultyNumber);
+		} 
+}
